1314a6d3cdeeef4eed2b8d12692ff2d0bbd3ac9c
Shereen2000 <tumisomokautu@gmail.com>
2024-07-07 17:43:08 +0200

    changed bills file

--- src/components/bills.js
+++ src/components/bills.js
@@ -11,7 +11,7 @@ const invoice = getInvoice(parseInt(params.invoiceId))
 
   return (
      <main className='flex flex-col py-5 my- w-1/3 mx-auto '>
-                <h2>Total Due</h2>
+                <h1>Total Due</h1>
                 <p>{invoice.name}</p>
                 <p>{invoice.amount}</p>
                 <p>{invoice.due}</p>
